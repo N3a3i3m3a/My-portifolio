6f8c137eae1c7c3da16c2b6b6185094792006f36
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-03-21 13:15:49 +0200

    responsive

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const bar = document.querySelector(".fa-bar");
-const menu = document.getElementById("menu")
+const body = document.querySelector("body")
+const hLinks = document.querySelectorAll("#hLink")
 
-bar.addEventListener("click", () => {
-  menu.classList.toggle("show-menu");
-});
+bar.addEventListener("click", ()=>{
+  menu.classList.toggle("hidden")
+  hamburger.classList.toggle("bg-white")
+})
+
+hLinks.forEach(link=>{
+  link.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    bar.classList.toggle("bg-white")
+  })
+})
 
